c7e5d24dcf71db2990eddbe6ffdb8e82a94ee383
tanvir.dpmiracle@gmail.com <latimglobal@gmail.com>
2018-09-11 18:23:50 +0600

    add custom module for fortune

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 //set up handlerbars view engine
@@ -21,17 +21,9 @@ app.get('/',function(req,res){
 });
 
 
-//create array of fortune cookies
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you do not know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-]
+
 app.get('/about', function(req,res){
-	var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-	res.render('about',{fortune: randomFortune});
+	res.render('about',{fortune: fortune.getFortune()});
 });
 
 //404 catch-all handler - middleware
